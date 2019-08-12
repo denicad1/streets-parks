15e7e3a72ea9bba53d9e641214196d843920a577
the_dude <denicad@msn.com>
2019-08-12 16:28:31 -0400

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let objs = (function () {
+
+    //this is just a super for the other classes. it really isn't needed but I did it anyway
     class basicInfo {
         constructor(name, buildYear) {
             this.name = name;
@@ -6,31 +8,36 @@ let objs = (function () {
         }
 
     }
-
+    //park obj
     class park extends basicInfo {
         constructor(name, buildYear, trees, parkArea) {
+            //basicInfo
             super(name, buildYear);
             this.trees = trees;
             this.parkArea = parkArea;
         };
+        //method for tree density
         treeDensity() {
             let density = this.trees / this.parkArea;
             return density;
         }
+        //method for park age
         parkAge() {
             let date = new Date().getFullYear - this.buildYear;
             return date;
         }
     }
+    //street obj
     class streets extends basicInfo {
         constructor(name, buildYear, streetLength, streetSize = 'normal') {
+            //basicInfo
             super(name, buildYear);
             this.streetLength = streetLength;
             this.streetSize = streetSize;
         }
 
     }
-    return {
+    return { //need to change these to set
         getPark: function (name, buildYear, trees, parkArea) {
             return new park(name, buildYear, trees, parkArea);
         },
@@ -39,3 +46,6 @@ let objs = (function () {
         }
     }
 })();
+/*may need to add arrays to add these objects too or maybe a map but i don't think a map would be the best choice.
+might need to add methods to streets class for calculations but I don't think they would fit there that weill
+*/
