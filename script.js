f9c03af332cdcc2a2d94dc8a85fa3e8fb22ca9b8
the_dude <denicad@msn.com>
2019-08-13 16:47:19 -0400

    got it working right

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let objs = (function () {
+{
 
     //this is just a super for the other classes. it really isn't needed but I did it anyway
     class basicInfo {
@@ -15,18 +15,19 @@ let objs = (function () {
             super(name, buildYear);
             this.trees = trees;
             this.parkArea = parkArea;
+            //calculate the current age of the parks
+            this.parkAge = () => {
+                let date = new Date().getFullYear() - this.buildYear;
+                return date;
+            }
         };
         //method for tree density
         treeDensity() {
             let density = this.trees / this.parkArea;
             return density;
         }
-        //method for park age
-        parkAge() {
-            let date = new Date().getFullYear - this.buildYear;
-            return date;
-        }
-    }
+
+    };
     //street obj
     class streets extends basicInfo {
         constructor(name, buildYear, streetLength, streetSize = 'normal') {
@@ -36,16 +37,71 @@ let objs = (function () {
             this.streetSize = streetSize;
         }
 
+    };
+    //arrays to hold either streets or parks
+    let arrOfParks = [];
+    let arrOfStreets = [];
+    let park1 = new park('park1', 1994, 2000, 2000);
+    let park2 = new park('park2', 1956, 950, 1200);
+    let park3 = new park('park3', 1820, 3500, 9000);
+    let street1 = new streets('birch', 1945, 1000, 'big');
+    let street2 = new streets('pine', 1933, 2000, 'small');
+    let street3 = new streets('elm', 1921, 2300, 'tiny');
+    let street4 = new streets('teek', 1974, 1000);
+    //populate arrays
+    function pushToArr(arrOf, ...objs) {
+        objs.forEach(cur => {
+            arrOf.push(cur);
+        })
+
     }
-    return { //need to change these to set
-        getPark: function (name, buildYear, trees, parkArea) {
-            return new park(name, buildYear, trees, parkArea);
-        },
-        getStreets: function (name, buildYear, streetLength, streetSize) {
-            return new streets(name, buildYear, streetLength, streetSize);
-        }
+    pushToArr(arrOfParks, park1, park2, park3);
+    pushToArr(arrOfStreets, street1, street2, street3, street4);
+    console.log(`------ Park Report -------------`)
+    //find average of all parks
+    function averageAgeOfParks(arrOfParks) {
+        let avg = arrOfParks.reduce((sum, el) => {
+            return sum + el.parkAge();
+        }, 0);
+
+        console.log(`The average age of all the parks is : ${(avg / arrOfParks.length).toFixed(2)}`);
     }
-})();
-/*may need to add arrays to add these objects too or maybe a map but i don't think a map would be the best choice.
-might need to add methods to streets class for calculations but I don't think they would fit there that weill
-*/
+    averageAgeOfParks(arrOfParks);
+    arrOfParks.forEach(cur => {
+        console.log(`the tree density for ${cur.name} is ${cur.treeDensity().toFixed(2)}`);
+    })
+
+
+    //find which parks have over 1000 trees
+    function treeAmountOver1000(arrOfParks) {
+        arrOfParks.forEach((cur) => {
+            (cur.trees > 1000) ? console.log(`${cur.name} has more than 1000 trees`): null;
+
+        });
+    }
+    treeAmountOver1000(arrOfParks);
+    // total length of streets. tried to use on parks but stopped trying to get that to work right now
+    function total(arr, param) {
+        let avg = arr.reduce((sum, el) => {
+
+            return sum + el[param];
+        }, 0);
+
+        return avg;
+    }
+
+    console.log(`-------- Streets Report --------- `);
+
+    //find the average for the length of the streets. want to get this to work for trees as well
+    function average(callback, arr) {
+        //callback function is total from above
+
+        console.log(`Our 4 streets have a total length of ${callback.toFixed(2)} and our average is ${(callback/arr.length).toFixed(2)}`);
+    }
+    //string is to select value from objects
+    average(total(arrOfStreets, 'streetLength'), arrOfStreets)
+    arrOfStreets.forEach(cur => {
+        console.log(`${cur.name}, built in ${cur.buildYear}, is a ${cur.streetSize} street`);
+    })
+
+};
